5c1b6037d14650b31cafd3fc36ce453b63ac6624
Jeramai <j.faber@provrex.nl>
2025-03-11 10:20:52 +0700

    refactor LoginPage component and add MobileButton for QR code login

--- components/LoginPage.tsx
+++ components/LoginPage.tsx
@@ -10,18 +10,6 @@ import { useEffect, useState } from 'react';
 import { Spinner } from './ui/spinner';
 
 export default function LoginPage() {
-  const [countdown, setCountdown] = useState(43);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      let newCountdown = countdown - 1;
-      if (countdown === 0) newCountdown = 120;
-      setCountdown(newCountdown);
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  }, [countdown]);
-
   return (
     <div className='min-h-screen flex flex-col items-center justify-center bg-gradient-to-b  text-white p-4'>
       <div className='w-full max-w-md'>
@@ -30,54 +18,16 @@ export default function LoginPage() {
           <h1 className='text-2xl font-bold ml-3'>Log in to Ronin NFT Trading</h1>
         </div>
 
-        <WayPointButton />
+        {/* <WayPointButton /> */}
         <ConnectRoninWalletButton />
 
-        <div className='flex items-center mb-6'>
+        {/* <div className='flex items-center mb-6'>
           <div className='flex-grow h-px bg-gray-700'></div>
           <span className='px-4 text-gray-400 text-sm'>OR</span>
           <div className='flex-grow h-px bg-gray-700'></div>
         </div>
 
-        <div className='text-center mb-4'>
-          <h2 className='text-lg font-medium mb-2'>Instant log in with QR code</h2>
-          <p className='text-gray-400 text-sm mb-4'>
-            Scan this QR code with{' '}
-            <Link href='#' className='text-blue-400'>
-              Ronin Mobile app
-            </Link>
-          </p>
-
-          <div className='flex justify-center mb-2'>
-            <div className='bg-white p-2 rounded-lg inline-block'>
-              <div className='relative w-48 h-48'>
-                <Image
-                  src='/placeholder.svg?height=200&width=200'
-                  alt='QR Code'
-                  width={200}
-                  height={200}
-                  className='rounded-md'
-                />
-                <div className='absolute inset-0 flex items-center justify-center'>
-                  <div className='bg-blue-500 p-1 rounded-md'>
-                    <div className='border-2 border-white p-1 rounded-md'>
-                      <div className='text-white'>
-                        <svg width='16' height='20' viewBox='0 0 16 20' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                          <path
-                            d='M8 0C3.6 0 0 3.6 0 8V16C0 18.2 1.8 20 4 20H12C14.2 20 16 18.2 16 16V8C16 3.6 12.4 0 8 0ZM12 16H4V8C4 5.8 5.8 4 8 4C10.2 4 12 5.8 12 8V16Z'
-                            fill='white'
-                          />
-                        </svg>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <p className='text-gray-400 text-sm'>Countdown refresh in {countdown}s</p>
-        </div>
+        <MobileButton /> */}
 
         <div className='text-center mt-8 mb-4'>
           <p className='text-gray-400 text-sm'>
@@ -92,7 +42,7 @@ export default function LoginPage() {
           <Info className='w-4 h-4 mr-1' />
           <p>
             By continuing, you agree to our{' '}
-            <Link href='#' className='text-blue-400'>
+            <Link href='/tos' className='text-blue-400'>
               Terms of Use
             </Link>
           </p>
@@ -116,7 +66,6 @@ function WayPointButton() {
     // setUser({ accounts });
   };
 
-  return null;
   return (
     <button
       className='w-full bg-blue-500 hover:bg-blue-600 text-white font-medium py-3 px-4 rounded-full mb-3'
@@ -153,26 +102,34 @@ function ConnectRoninWalletButton() {
     try {
       if (!connector && error === ConnectorErrorType.PROVIDER_NOT_FOUND) {
         window.open('https://wallet.roninchain.com', '_blank');
-        return;
+        throw new Error('No wallet provider found');
       }
 
       const chainId = Number(process.env.NEXT_PUBLIC_CHAIN_ID) || 2020;
       const connectResult = await connector?.connect(chainId);
       if (!connectResult) {
-        return;
+        throw new Error('No connection results');
       }
 
       const connectedAddress = connectResult.account;
       const currentChainId = connectResult.chainId;
 
       const userAddresses = await connector?.getAccounts();
-      if (userAddresses) {
-        setUser({
-          connectedAddress,
-          userAddresses,
-          currentChainId
-        });
+      if (!userAddresses) {
+        throw new Error('No user addresses found');
       }
+
+      const approvedAddresses = await connector?.requestAccounts();
+      if (!approvedAddresses) {
+        throw new Error('No addresses have been approved');
+      }
+
+      setUser({
+        connectedAddress,
+        userAddresses,
+        currentChainId,
+        approvedAddresses
+      });
     } catch (e) {
       console.error(e);
     } finally {
@@ -195,5 +152,54 @@ function ConnectRoninWalletButton() {
     >
       {isLoading ? <Spinner /> : 'Connect with Ronin Wallet'}
     </button>
+  );
+}
+function MobileButton() {
+  const [countdown, setCountdown] = useState(60);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      let newCountdown = countdown - 1;
+      if (countdown === 0) newCountdown = 120;
+      setCountdown(newCountdown);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [countdown]);
+
+  return (
+    <div className='text-center mb-4'>
+      <h2 className='text-lg font-medium mb-2'>Instant log in with QR code</h2>
+      <p className='text-gray-400 text-sm mb-4'>
+        Scan this QR code with{' '}
+        <Link href='#' className='text-blue-400'>
+          Ronin Mobile app
+        </Link>
+      </p>
+
+      <div className='flex justify-center mb-2'>
+        <div className='bg-white p-2 rounded-lg inline-block'>
+          <div className='relative w-48 h-48'>
+            <Image src='/placeholder.svg?height=200&width=200' alt='QR Code' width={200} height={200} className='rounded-md' />
+            <div className='absolute inset-0 flex items-center justify-center'>
+              <div className='bg-blue-500 p-1 rounded-md'>
+                <div className='border-2 border-white p-1 rounded-md'>
+                  <div className='text-white'>
+                    <svg width='16' height='20' viewBox='0 0 16 20' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                      <path
+                        d='M8 0C3.6 0 0 3.6 0 8V16C0 18.2 1.8 20 4 20H12C14.2 20 16 18.2 16 16V8C16 3.6 12.4 0 8 0ZM12 16H4V8C4 5.8 5.8 4 8 4C10.2 4 12 5.8 12 8V16Z'
+                        fill='white'
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <p className='text-gray-400 text-sm'>Countdown refresh in {countdown}s</p>
+    </div>
   );
 }
--- hooks/use-store.ts
+++ hooks/use-store.ts
@@ -4,6 +4,7 @@ import { persist } from 'zustand/middleware';
 interface User {
   connectedAddress: string;
   userAddresses: string[];
+  approvedAddresses: string[];
   currentChainId: number;
 }
 interface MainStorage {
