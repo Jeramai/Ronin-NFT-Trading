d3579f2a0c2fbb65889901afbd32308bd740ecae
Jeramai <j.faber@provrex.nl>
2025-03-13 13:04:18 +0700

    Working version of the urlPrefix

--- lib/urlPrefix.ts
+++ lib/urlPrefix.ts
@@ -2,8 +2,7 @@ export function getUrlPrefix(): string {
   let prefix = '';
 
   if (process.env.GITHUB_ACTIONS) {
-    prefix = '/Ronin-NFT-Trading';
-    // prefix = (process.env.GITHUB_REPOSITORY ?? '').replace(/.*?\//, '');
+    prefix = '/' + (process.env.GITHUB_REPOSITORY ?? '').replace(/.*?\//, '');
   }
 
   return prefix;
