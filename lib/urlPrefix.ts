20684049231303aa641e062db91eb822b3789eb6
Jeramai <j.faber@provrex.nl>
2025-03-13 13:19:56 +0700

    I'm losing it 🎶

--- lib/urlPrefix.ts
+++ lib/urlPrefix.ts
@@ -2,7 +2,8 @@ export function getUrlPrefix(): string {
   let prefix = '';
 
   if (process.env.NODE_ENV !== 'development') {
-    prefix = '/' + (process.env.GITHUB_REPOSITORY ?? '').replace(/.*?\//, '');
+    prefix = '/Ronin-NFT_Trading';
+    // prefix = '/' + (process.env.GITHUB_REPOSITORY ?? '').replace(/.*?\//, '');
   }
 
   return prefix;
