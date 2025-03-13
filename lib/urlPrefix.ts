726e32db00a9ea4a33b0ec2337dbe07fcf716845
Jeramai <j.faber@provrex.nl>
2025-03-13 13:01:13 +0700

    ...

--- lib/urlPrefix.ts
+++ lib/urlPrefix.ts
@@ -2,6 +2,7 @@ export function getUrlPrefix(): string {
   let prefix = '';
 
   if (process.env.GITHUB_ACTIONS) {
+    prefix = '/Ronin-NFT-Trading';
     // prefix = (process.env.GITHUB_REPOSITORY ?? '').replace(/.*?\//, '');
   }
 
