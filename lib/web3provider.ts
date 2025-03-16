28fdac415ba67c76291ad31ce30683b5c1e159ca
Jeramai <j.faber@provrex.nl>
2025-03-16 14:16:14 +0700

    Use the ronin wallet to initiate the contract with

--- lib/web3provider.ts
+++ lib/web3provider.ts
@@ -1,34 +1,40 @@
-'use server';
+import { ChainIds, requestRoninWalletConnector } from '@sky-mavis/tanto-connect';
+import { BrowserProvider, Contract } from 'ethers';
+import ABI from './ABI';
 
-import ABI from '@/lib/ABI';
-import { AddressLike, Contract, JsonRpcProvider, Wallet } from 'ethers';
-
-export async function tradingProvider() {
-  const RONIN_RPC_URL =
-    process.env.NEXT_PUBLIC_CHAIN_ID == '2020' ? 'https://api.roninchain.com/rpc' : 'https://saigon-testnet.roninchain.com/rpc';
-  return new JsonRpcProvider(RONIN_RPC_URL);
-}
-export async function tradingWallet() {
-  const provider = await tradingProvider();
-  const privateKey = process.env.WALLET_PRIVATE_KEY as string;
-  return new Wallet(privateKey, provider);
-}
-export async function tradingContract() {
-  const wallet = await tradingWallet();
-  return new Contract(process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as string, ABI, wallet);
+async function connectWallet() {
+  const chainId = process.env.NEXT_PUBLIC_CHAIN_ID == '2020' ? ChainIds.RoninMainnet : ChainIds.RoninTestnet;
+  const connector = await requestRoninWalletConnector();
+  return connector.connect(chainId);
 }
 
+async function getContract() {
+  const wallet = await connectWallet();
+  const provider = new BrowserProvider(wallet.provider);
+  const signer = await provider.getSigner(wallet.account);
+
+  return new Contract(process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as string, ABI, signer);
+}
+
+export async function proposeTrade(trader: string) {
+  const contract = await getContract();
+
+  try {
+    // Send the transaction
+    const tx = await contract.proposeTrade(trader);
+    // Wait for transaction to be mined
+    const receipt = await tx.wait();
+
+    // Get the trade ID from the event
+    const event = receipt.logs[0];
+    const parsedEvent = contract.interface.parseLog(event);
+    return parseInt(parsedEvent?.args?.tradeId);
+  } catch (error) {
+    console.error('Error proposing trade:', error);
+    throw error;
+  }
+}
 export async function getTrade(tradeIndex: number) {
-  const contract = await tradingContract();
+  const contract = await getContract();
   return contract.getTrade(tradeIndex);
-}
-export async function proposeTrade(trader: AddressLike) {
-  const contract = await tradingContract();
-  const tx = await contract.proposeTrade(trader);
-  const receipt = await tx.wait();
-
-  // Try and get the tradeId from the event
-  const event = receipt.logs[0];
-  const parsedEvent = contract.interface.parseLog(event);
-  return parseInt(parsedEvent?.args?.tradeId);
 }
